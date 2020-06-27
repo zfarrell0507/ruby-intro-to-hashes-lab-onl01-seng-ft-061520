ec6564f6cbe8c1e044f3fa2b1716645e8115a449
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 18:47:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,6 +44,6 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	#copy ait here!
 
 end
