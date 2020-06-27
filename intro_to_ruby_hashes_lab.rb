8e3fea80d591d7a05715a9ef68b1a28e9dfeaad1
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 18:48:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -44,6 +44,37 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	#copy ait here!
+	monopoly = {
+  :railroads =>
+  {
+    :pieces => 4,
+    :rent_in_dollars =>
+    {
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200
+    },
+    :names=>
+    {
+      :reading_railroad =>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad =>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad =>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline =>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
 
 end
