a1e6ad0e418b917715f01a9026b97968e0ff9988
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 18:49:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,20 +26,38 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {}
-	monopoly[:railroads] = {}
-  monopoly[:railroads] [:pieces] = 4
-  monopoly[:railroads] [:names] = {}
-  monopoly[:railroads] [:rent_in_dollars] = {}
-  monopoly[:railroads] [:rent_in_dollars] [:one_piece_owned] = 25
-  monopoly[:railroads] [:rent_in_dollars] [:two_pieces_owned] = 50
-  monopoly[:railroads] [:rent_in_dollars] [:three_pieces_owned] = 100
-  monopoly[:railroads] [:rent_in_dollars] [:four_pieces_owned] = 200
-  monopoly[:railroads] [:name] [:reading_railroad] = {}
-  monopoly[:railroads] [:name] [:pennsylvania_railroad] = {}
-  monopoly[:railroads] [:name] [:b_and_o_railroad] = {}
-  monopoly[:railroads] [:name] [:shortline] = {}
-  monopoly
+monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad=>
+      {
+        "mortgage_value"=>"$200"
+      },
+      :b_and_o_railroad=>
+      {
+        "mortgage_value"=>"$400"
+      },
+      :shortline=>
+      {
+        "mortgage_value"=>"$800"
+      }
+    }
+  }
+}
 
 end
 
