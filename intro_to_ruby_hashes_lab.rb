35681b3965c97c5db94115726776af3f8feffc01
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-27 18:32:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,7 @@ def monopoly_with_third_tier
   monopoly[:railroads] [:rent_in_dollars] = {}
   monopoly[:railroads] [:rent_in_dollars] [:one_piece_owned] = 25
   monopoly[:railroads] [:rent_in_dollars] [:one_piece_owned] = 25
-  
+  monopoly[:railroads] [:rent_in_dollars] [:one_piece_owned] = 25
 
 end
 
